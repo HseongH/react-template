4cc05cc820fb9e11e592a44bd6c6f090d96e37e3
HseongH <hh4518@gmail.com>
2024-05-21 00:41:43 +0900

    디자인 시스템을 mui에서 bootstrap으로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 
-import CssBaseline from '@mui/material/CssBaseline';
-import { Box } from '@mui/material';
-import Header from './components/Header';
-import Drawer from './components/Header/Drawer';
-
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <CssBaseline enableColorScheme />
-
-        <Header />
-      </RecoilRoot>
+      <RecoilRoot />
     </QueryClientProvider>
   );
 }
