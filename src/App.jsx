a6b634a3841383b888f1d4218181939d2d73b8f1
SH-Hong1 <hongseonghun@mz.co.kr>
2024-06-20 18:15:43 +0900

    lightblue for react 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,58 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RecoilRoot } from 'recoil';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Switch, Route, Redirect } from 'react-router';
+import { HashRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-import GlobalStyle from './style/GlobalStyle';
+/* eslint-disable */
+import ErrorPage from '@/pages/error/ErrorPage';
+/* eslint-enable */
 
-import Header from './components/Header';
+import './styles/theme.scss';
+import Layout from './components/Layout';
+import Login from '@/pages/login/Login';
+import Register from '@/pages/register/Register';
+import { logoutUser } from '@/actions/user';
 
-const queryClient = new QueryClient();
-
-function App() {
+function PrivateRoute({ dispatch, component, ...rest }) {
+  if (!Login.isAuthenticated(JSON.parse(localStorage.getItem('authenticated')))) {
+    dispatch(logoutUser());
+    return <Redirect to="/login" />;
+  }
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <GlobalStyle>
-          <Header />
-        </GlobalStyle>
-      </RecoilRoot>
-    </QueryClientProvider>
+    // eslint-disable-line
+    <Route {...rest} render={(props) => React.createElement(component, props)} />
   );
 }
 
-export default App;
+function CloseButton({ closeToast }) {
+  return <i onClick={closeToast} className="la la-close notifications-close" />;
+}
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <ToastContainer autoClose={5000} hideProgressBar closeButton={<CloseButton />} />
+        <HashRouter>
+          <Switch>
+            <Route path="/" exact render={() => <Redirect to="/app/main" />} />
+            <Route path="/app" exact render={() => <Redirect to="/app/main" />} />
+            <PrivateRoute path="/app" dispatch={this.props.dispatch} component={Layout} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/error" exact component={ErrorPage} />
+            <Route component={ErrorPage} />
+            <Redirect from="*" to="/app/main/dashboard" />
+          </Switch>
+        </HashRouter>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
-import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import * as serviceWorker from './serviceWorker';
+
 import App from '@/App';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import reducers from './reducers';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-]);
+const store = createStore(reducers, applyMiddleware(ReduxThunk));
 
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
